1b2ce7277181a356ef89ec22bac815c1cc37609f
Emidio Croci <emidio.croci@gmail.com>
2013-04-12 07:22:34 +0200

    Changed some application properties
    Now it uses stylus instead of less. Restored dependencies

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.methodOverride());
   app.use(express.cookieParser('your secret here'));
   app.use(express.session());
 app.use(app.router);
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
+  app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
