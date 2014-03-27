565ab77a77f2332f451d4f214ca39b4565a9dadb
Emidio Croci <emidio.croci@gmail.com>
2014-03-27 13:57:39 +0100

    Revised layout with bootstrap

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -11,7 +11,7 @@ angular.module('scrello', [
   'ui.date'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/sprint', {templateUrl: 'partials/sprint', controller: 'SprintCtrl'});
+  $routeProvider.when('/', {templateUrl: 'partials/sprint', controller: 'SprintCtrl'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
